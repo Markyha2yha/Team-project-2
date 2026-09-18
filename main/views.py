b0ad44badba473b6302a7f7536b6547568cd7716
Марк <rybalkon24@gmail.com>
2026-09-18 20:27:19 +0300

    The base is ready

---
+++ main/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'main/home.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def contacts(request):
+    return render(request, 'main/home.html')
